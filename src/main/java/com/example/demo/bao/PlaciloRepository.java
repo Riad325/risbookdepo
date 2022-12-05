982eb00f4c9b5248e0fc5d2531427d1b78abb3c3
katarina <katarina.lipovsek2@student.um.si>
2022-12-05 11:25:44 +0100

    popravek pri repozitorijih

--- src/main/java/com/example/demo/bao/PlaciloRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.bao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.vao.Placilo;
-
-public interface PlaciloRepository extends CrudRepository<Placilo, Long> { 
-    
-}
